9a48ddad73489732ec02d45edeef8c66aa605aa0
Augusto Nunes <augusto.cesar@gmail.com>
2016-08-16 11:47:24 -0300

    Mudei de ideia!

--- ksc.R
+++ ksc.R
@@ -21,11 +21,14 @@ for(i in 1:nrow(dados)){
   mat_dist_ind[,i] <- head(order(mat_dist[,i]), -1)
 }
 
-
+llr_z <- vector()
 # Scan Circular
 for(i in 1:nrow(dados)){
   n_z <- dados[i, 4]
   c_z <- dados[i, 1]
-
-  }
+  mu_z <- casos_total*(n_z/pop_total)
+  ifelse(c_z > mu_z,
+         llr_z[i] <- c_z*log(c_z/mu_z) + (casos_total - c_z)*log((casos_total - c_z)/(casos_total - mu_z)),
+         0)
+}
 

3f3918a257301f8135a19cda0ae5a9aa3908ce55
Augusto Nunes <augusto.cesar@gmail.com>
2016-08-18 11:51:10 -0300

    bli bli bli

--- ksc.R
+++ ksc.R
@@ -16,19 +16,45 @@ mat_dist <- as.matrix(mat_dist)
 diag(mat_dist) = NA
 mat_dist_ind <- matrix(ncol = nrow(dados), nrow = nrow(dados) - 1)
 
-# Gerando a matriz de distancias ordenada por indices 
-for(i in 1:nrow(dados)){
-  mat_dist_ind[,i] <- head(order(mat_dist[,i]), -1)
+# Gerando a matriz de distancias ordenada por indices
+for (i in 1:nrow(dados)) {
+  mat_dist_ind[, i] <- head(order(mat_dist[, i]),-1)
 }
 
-llr_z <- vector()
+llr_z <- numeric()
+
 # Scan Circular
-for(i in 1:nrow(dados)){
+for (i in 1:nrow(dados)) {
+  zona <- i
   n_z <- dados[i, 4]
   c_z <- dados[i, 1]
-  mu_z <- casos_total*(n_z/pop_total)
+  mu_z <- casos_total * (n_z / pop_total)
   ifelse(c_z > mu_z,
-         llr_z[i] <- c_z*log(c_z/mu_z) + (casos_total - c_z)*log((casos_total - c_z)/(casos_total - mu_z)),
-         0)
+         llr_z <-
+           c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
+                                                               (casos_total - mu_z)),
+         llr_z <- 0)
+  mat_llr <- cbind(i, llr_z[i])
+  while (n_z < pop_total / 2) {
+    for (j in mat_dist_ind[i]) {
+      zona <<- append(zona, j)
+      n_z <- sum(n_z, dados[j, 4])
+      c_z <- sum(c_z, dados[j, 1])
+      mu_z <- casos_total * (n_z / pop_total)
+      if (c_z > mu_z) {
+        llr_z <-
+          c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
+                                                              (casos_total - mu_z))
+        mat_llr <- rbind(mat_llr, append(zona, llr_z))
+        break()
+      }
+      else{
+          mat_llr <- rbind(mat_llr, append(zona, 0))
+        }
+      # ifelse(,
+      #        mat_llr <- rbind(mat_llr,append(zona, llr_z[i])),
+      #        next())
+      
+    }
+  }
 }
-

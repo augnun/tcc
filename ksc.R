1176e41f5d4cf9681f7b200a276ea0099156ecf8
Augusto Nunes <augusto.cesar@gmail.com>
2016-08-29 17:04:09 -0300

    bla bla bla

--- ksc.R
+++ ksc.R
@@ -13,7 +13,7 @@ casos_total <- sum(dados$Casos)
 mat_dist <- dist(dados[, 2:3])
 mat_dist <- as.matrix(mat_dist)
 
-# diag(mat_dist) <- NA
+diag(mat_dist) <- NA
 mat_dist_ind <- matrix(ncol = nrow(dados), nrow = nrow(dados) - 1)
 
 # Gerando a matriz de distancias ordenada por indices
@@ -25,80 +25,39 @@ for (i in 1:nrow(dados)) {
 df.llr_z <- data.frame()
 
 # Scan Circular
-# for (i in 1:nrow(dados)) {
-#   zona <- i
-#   n_z <- dados[i, 4]
-#   c_z <- dados[i, 1]
-#   mu_z <- casos_total * (n_z / pop_total)
-#   ifelse(c_z > mu_z,
-#          llr_z <-
-#            c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
-#                                                                (casos_total - mu_z)),
-#           llr_z <- 0)
-#    df.llr_z <- rbind(df.llr_z,(append(zona, llr_z)))
-#  }
+for (i in 1:nrow(dados)) {
+  zona <- i
+  n_z <- dados[i, 4]
+  c_z <- dados[i, 1]
+  mu_z <- casos_total * (n_z / pop_total)
+  ifelse(c_z > mu_z,
+         llr_z <-
+           c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
+                                                               (casos_total - mu_z)),
+          llr_z <- 0)
+   df.llr_z <- rbind(df.llr_z,(append(zona, llr_z)))
+ }
 
+zonas <- array()
 
-# colnames(df.llr_z) <- c("zonas", "llr")
-# df.llr_z$zonas <- as.list(df.llr_z$zonas)
-tamanho <- 1
-resultado <- data.frame()
-
-
-# for(i in 1:nrow(df.llr_z)){
-#   zona <- i
-#   # zona <- character()
-#   for(j in mat_dist_ind[,i]){
-#     zona <- append(zona, j)
-#     n_z <- sum(dados[c(noquote(as.vector(zona))), 4])
-#     if(n_z > pop_total/2){next()}
-#     else{
-#     c_z <- sum(dados[c(noquote(as.vector(zona))), 1])
-#     mu_z <- casos_total*(n_z/pop_total)
-#     ifelse(c_z > mu_z,
-#            llr_z <-
-#              c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
-#                                                                  (casos_total - mu_z)),
-#            llr_z <- 0)
-#     paste(zona, llr_z)
-# 
-#   }
-#   }
-# }
-# resultado <- data.frame()
-# for(i in 1:nrow(mat_dist)){
-#   zona <- i
-#   n_z <- sum(dados[i,4])
-#   if(n_z > pop_total/2){next()}
-#   else{
-#     c_z <- sum(dados[i,1])
-#     mu_z <- casos_total*(n_z/pop_total)
-#     ifelse(c_z > mu_z,
-#           llr_z <-
-#             c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
-#                                                                 (casos_total - mu_z)),
-#           llr_z <- 0)
-#     df.llr_z <- rbind(df.llr_z, c(zona, llr_z))
-#   }
-#   for(j in i+1:nrow(mat_dist)){
-# 
-#     zona <- c( zona, j)
-#     n_z <- sum(dados[zona,4])
-#     if(n_z > pop_total/2){next()}
-#     else{
-#       c_z <- sum(dados[zona,1])
-#       mu_z <- casos_total*(n_z/pop_total)
-#       ifelse(c_z > mu_z,
-#              llr_z <-
-#                c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
-#                                                                    (casos_total - mu_z)),
-#              llr_z <- 0)
-#       resultado <-  c(factor(zona), llr_z)
-#       df.llr_z <- rbind(df.llr_z, factor(resultado))
-#   }
-# }
-# }
-# 
-#   
-  
-
+for(i in 1:nrow(dados)){
+  zona <- c(i)
+  for(j in mat_dist_ind[,i]){
+    zona <- c(zona,j)
+    n_z <- sum(dados[zona, 4])
+    if(n_z > pop_total/2){
+      zona <- zona[1:length(zona)-1]
+      next()
+      }
+    c_z <- sum(dados[zona, 1])
+    mu_z <- casos_total * (n_z / pop_total)
+    ifelse(c_z > mu_z,
+           llr_z[i] <-
+             c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
+                                                                 (casos_total - mu_z)),
+           llr_z[i] <- 0)
+    zonas <- cbind(zona)
+    # resultado[i] <- data.frame(x = llr_z, y = I(zonas))
+    
+  }
+}

470990b2def10adf3426ea31b84adac0f39805a7
Augusto Nunes <augusto.cesar@gmail.com>
2016-08-29 15:42:09 -0300

    mnm

--- ksc.R
+++ ksc.R
@@ -25,50 +25,80 @@ for (i in 1:nrow(dados)) {
 df.llr_z <- data.frame()
 
 # Scan Circular
-for (i in 1:nrow(dados)) {
-  zona <- i
-  n_z <- dados[i, 4]
-  c_z <- dados[i, 1]
-  mu_z <- casos_total * (n_z / pop_total)
-  ifelse(c_z > mu_z,
-         llr_z <-
-           c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
-                                                               (casos_total - mu_z)),
-          llr_z <- 0)
-   df.llr_z <- rbind(df.llr_z,(append(zona, llr_z)))
- }
+# for (i in 1:nrow(dados)) {
+#   zona <- i
+#   n_z <- dados[i, 4]
+#   c_z <- dados[i, 1]
+#   mu_z <- casos_total * (n_z / pop_total)
+#   ifelse(c_z > mu_z,
+#          llr_z <-
+#            c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
+#                                                                (casos_total - mu_z)),
+#           llr_z <- 0)
+#    df.llr_z <- rbind(df.llr_z,(append(zona, llr_z)))
+#  }
 
 
-colnames(df.llr_z) <- c("zonas", "llr")
-df.llr_z$zonas <- as.list(df.llr_z$zonas)
+# colnames(df.llr_z) <- c("zonas", "llr")
+# df.llr_z$zonas <- as.list(df.llr_z$zonas)
 tamanho <- 1
 resultado <- data.frame()
 
 
-for(i in 1:nrow(df.llr_z)){
-  zona <- i
-  # zona <- character()
-  for(j in mat_dist_ind[,i]){
-    zona <- append(zona, j)
-    n_z <- sum(dados[c(noquote(as.vector(zona))), 4])
-    if(n_z > pop_total/2){next()}
-    else{
-    c_z <- sum(dados[c(noquote(as.vector(zona))), 1])
-    mu_z <- casos_total*(n_z/pop_total)
-    ifelse(c_z > mu_z,
-           llr_z <-
-             c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
-                                                                 (casos_total - mu_z)),
-           llr_z <- 0)
-    df.llr_z <- rbind(zona, llr_z)
-    # resultado <- cbind(as.list(zona), llr_z)
-    df.llr_z <- rbind(df.llr_z, resultado[1:length(resultado) -1])
-    # df.llr_z <- rbind(df.llr_z,paste((as.character(zona) llr_z)))
-  }
-  }
-}
-
-
-  
+# for(i in 1:nrow(df.llr_z)){
+#   zona <- i
+#   # zona <- character()
+#   for(j in mat_dist_ind[,i]){
+#     zona <- append(zona, j)
+#     n_z <- sum(dados[c(noquote(as.vector(zona))), 4])
+#     if(n_z > pop_total/2){next()}
+#     else{
+#     c_z <- sum(dados[c(noquote(as.vector(zona))), 1])
+#     mu_z <- casos_total*(n_z/pop_total)
+#     ifelse(c_z > mu_z,
+#            llr_z <-
+#              c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
+#                                                                  (casos_total - mu_z)),
+#            llr_z <- 0)
+#     paste(zona, llr_z)
+# 
+#   }
+#   }
+# }
+# resultado <- data.frame()
+# for(i in 1:nrow(mat_dist)){
+#   zona <- i
+#   n_z <- sum(dados[i,4])
+#   if(n_z > pop_total/2){next()}
+#   else{
+#     c_z <- sum(dados[i,1])
+#     mu_z <- casos_total*(n_z/pop_total)
+#     ifelse(c_z > mu_z,
+#           llr_z <-
+#             c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
+#                                                                 (casos_total - mu_z)),
+#           llr_z <- 0)
+#     df.llr_z <- rbind(df.llr_z, c(zona, llr_z))
+#   }
+#   for(j in i+1:nrow(mat_dist)){
+# 
+#     zona <- c( zona, j)
+#     n_z <- sum(dados[zona,4])
+#     if(n_z > pop_total/2){next()}
+#     else{
+#       c_z <- sum(dados[zona,1])
+#       mu_z <- casos_total*(n_z/pop_total)
+#       ifelse(c_z > mu_z,
+#              llr_z <-
+#                c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
+#                                                                    (casos_total - mu_z)),
+#              llr_z <- 0)
+#       resultado <-  c(factor(zona), llr_z)
+#       df.llr_z <- rbind(df.llr_z, factor(resultado))
+#   }
+# }
+# }
+# 
+#   
   
 

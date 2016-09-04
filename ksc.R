db75269a762a491b1d93bb515859369a6180460f
Augusto Cesar Nunes <augusto.cesar@gmail.com>
2016-09-04 08:59:56 -0300

    DEU CERTO

--- ksc.R
+++ ksc.R
@@ -61,3 +61,26 @@ for(i in 1:nrow(dados)){
     
   }
 }
+
+resultado <- data.frame()
+
+for(i in 1:ncol(mat_dist_ind)){
+  zona <- i
+  for(j in mat_dist_ind[,i]){
+    zona <- append(zona,j)
+    n_z <- sum(dados[zona, 4])
+    if(n_z > pop_total/2){
+      zona <- zona[1:length(zona)-1]
+      next()
+    }
+    c_z <- sum(dados[zona, 1])
+    mu_z <- casos_total * (n_z / pop_total)
+    ifelse(c_z > mu_z,
+           llr_z <-
+             c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
+                                                                 (casos_total - mu_z)),
+           llr_z <- 0)
+    resultado <- rbind(resultado, cbind(list(zona), llr_z))
+    
+  }
+}

758cbe268f701c45289c8596723de70aa9b814cb
Augusto Nunes <augusto.cesar@gmail.com>
2016-10-17 17:20:29 -0200

    Após reunião

--- pso.R
+++ pso.R
@@ -21,49 +21,87 @@ for (i in 1:nrow(dados)) {
 # Matriz de adjacências 
 mat.adj <- read.csv("hex.adj", sep = "", header=FALSE)
 
-resultado <- 0
-vizinhos <- data.frame(which(mat.adj == 1, arr.ind = T))
+# resultado <- 0
+# vizinhos <- list()
+# 
+# for (i in vizinhos$col){
+# 
+#   for (j in subset(vizinhos, vizinhos$col == i)$row){
+#     zona <- append(i, j)
+#     n_z <- sum(dados[zona, 4])
+#     if(n_z > pop_total/2){
+#       zona <- zona[1:length(zona)-1]
+#       next()
+#     }
+#     c_z <- sum(dados[zona, 1])
+#     mu_z <- casos_total * (n_z / pop_total)
+#     ifelse(c_z > mu_z,
+#            llr_z <-
+#              c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
+#                                                                  (casos_total - mu_z)),
+#            llr_z <- 0)
+#     if(llr_z > resultado){
+#     resultado <-   llr_z
+#     zonas <- zona
+#     }
+#     else{  next()}
+#   }
+#     for (j in subset(vizinhos, vizinhos$col == zonas[-1])$row){
+#       zona <- append(zona, j)
+#       n_z <- sum(dados[zona, 4])
+#       if(n_z > pop_total/2){
+#         zona <- zona[1:length(zona)-1]
+#         next()
+#       }
+#       c_z <- sum(dados[zona, 1])
+#       mu_z <- casos_total * (n_z / pop_total)
+#       ifelse(c_z > mu_z,
+#              llr_z <-
+#                c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
+#                                                                    (casos_total - mu_z)),
+#              llr_z <- 0)
+#       if(llr_z > resultado){
+#         resultado <-   llr_z
+#         zonas <- zona
+#       }
+#   }
+# }
 
-for (i in vizinhos$col){
-  
-  for (j in subset(vizinhos, vizinhos$col == i)$row){
-    zona <- append(i, j)
+vizinhos <- lapply(mat.adj, function(x) which(x == 1, arr.ind = T))
+resultado <- data.frame()
+zonas_candidatas <- data.frame()
+
+for(i in 1:nrow(dados)){
+  zona <- i
+  n_z <- dados[i,4]
+  if(n_z > pop_total/2){next()}
+  c_z <- dados[i,1]
+  mu_z <- casos_total * (n_z / pop_total)
+  ifelse(c_z > mu_z,
+         llr_z <-
+           c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
+                                                               (casos_total - mu_z)),
+         llr_z <- 0)
+  resultado <- rbind(resultado,cbind(zona, llr_z))
+  for(j in 1:nrow(dados)){
+    for(k in 1:length(vizinhos[[j]])){
+    zona <- unique(append(zona, vizinhos[[j]][k]))
     n_z <- sum(dados[zona, 4])
-    if(n_z > pop_total/2){
-      zona <- zona[1:length(zona)-1]
-      next()
-    }
+    if(n_z > pop_total/2){next()}
     c_z <- sum(dados[zona, 1])
     mu_z <- casos_total * (n_z / pop_total)
-    ifelse(c_z > mu_z,
-           llr_z <-
-             c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
-                                                                 (casos_total - mu_z)),
-           llr_z <- 0)
-    if(llr_z > resultado){
-    resultado <-   llr_z
-    zonas <- zona
+    if(c_z > mu_z){
+           llr_z <- append(llr_z, c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
+                                                                 (casos_total - mu_z)))
+           zonas_candidatas <- rbind(zonas_candidatas,cbind(zona, llr_z))
+           
     }
-    else{  next()}
-  }
-    for (j in subset(vizinhos, vizinhos$col == zonas[-1])$row){
-      zona <- append(zona, j)
-      n_z <- sum(dados[zona, 4])
-      if(n_z > pop_total/2){
-        zona <- zona[1:length(zona)-1]
-        next()
-      }
-      c_z <- sum(dados[zona, 1])
-      mu_z <- casos_total * (n_z / pop_total)
-      ifelse(c_z > mu_z,
-             llr_z <-
-               c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
-                                                                   (casos_total - mu_z)),
-             llr_z <- 0)
-      if(llr_z > resultado){
-        resultado <-   llr_z
-        zonas <- zona
-      }
-  }
-  
+    else{
+           llr_z <- append(llr_z, 0)
+           zonas_candidatas <- rbind(zonas_candidatas,I(zona, llr_z))
+    }
+    
+    }
+    
+}
 }

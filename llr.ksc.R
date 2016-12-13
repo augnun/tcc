00e3d8e5d78e164f6171a006cb0a6330d2ec3075
Augusto Nunes <augusto.cesar@gmail.com>
2016-12-13 15:07:17 -0200

    Deu certo! Bem mais rápido!

--- llr.ksc.R
+++ llr.ksc.R
@@ -1,10 +1,11 @@
 # Inicialização dados
+# arquivos <- list.files("dados_hex", full.names = TRUE)
+# dados <- lapply(arquivos, read.table, header = FALSE)
+# dados <- data.frame(dados)
+# names(dados) <- c("Casos", "X_c", "Y_c", "Pop")
 
-arquivos <- list.files("dados_hex", full.names = TRUE)
-dados <- lapply(arquivos, read.table, header = FALSE)
-dados <- data.frame(dados)
-names(dados) <- c("Casos", "X_c", "Y_c", "Pop")
 
+dados <- read.csv("dados_hex/dados.csv", header = T)
 #' Title llr.ksc
 #'
 #' @param dados: data-frame com quatro colunas: casos,
@@ -21,7 +22,7 @@ llr.ksc <- function(dados) {
   
   # Matriz de distâncias
   
-  mat_dist <- dist(dados[, 2:3])
+  mat_dist <- dist(dados[, 3:4])
   mat_dist <- as.matrix(mat_dist)
   
   diag(mat_dist) <- NA
@@ -34,26 +35,34 @@ llr.ksc <- function(dados) {
   
   
   resultado <- data.frame()
+  llr_z <- vector(length = nrow(dados)^2)
+  zonas <- list(length = nrow(dados^2))
+  k = 1
   
   for (i in 1:ncol(mat_dist_ind)) {
     zona <- i
     for (j in mat_dist_ind[, i]) {
       zona <- append(zona, j)
-      n_z <- sum(dados[zona, 4])
+      n_z <- sum(dados[zona, 5])
       if (n_z > pop_total / 2) {
         zona <- zona[1:length(zona) - 1]
         next()
       }
-      c_z <- sum(dados[zona, 1])
+      c_z <- sum(dados[zona, 2])
       mu_z <- casos_total * (n_z / pop_total)
       ifelse(c_z > mu_z,
-             llr_z <-
+             llr_z[k] <-
                c_z * log(c_z / mu_z) + (casos_total - c_z) * log((casos_total - c_z) /
                                                                    (casos_total - mu_z)),
-             llr_z <- 0)
-      resultado <- rbind(resultado, cbind(list(zona), llr_z))
-      
+             llr_z[k] <- 0)
+      zonas[k] <-  list(zona)
+      k <- k + 1
     }
   }
+  which.max(llr_z)
+  resultado <- list()
+  resultado <- list(c(zonas[which.max(llr_z)]),max(llr_z))
   return(resultado)
 }
+
+llr.ksc(dados)
